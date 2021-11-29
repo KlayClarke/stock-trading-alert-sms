b617a69d2f4793baf358b6663591bde6adff18b1
Klay Clarke <klayaclarke@gmail.com>
2021-11-29 11:11:54 -0500

    retrieve daily stock data for TSLA use itertools package to retrieve data (closing prices) from last two days subtract closing prices from each other to get price change

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
+import os
+import requests
+import itertools
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
+AV_API_KEY = os.environ.get('AV_API_KEY')
+NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
+STOCK_ENDPOINT = 'https://www.alphavantage.co/query'
+NEWS_ENDPOINT = 'https://newsapi.org/v2/everything'
+
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+
+parameters = {
+    'function': 'TIME_SERIES_DAILY_ADJUSTED',
+    'symbol': STOCK,
+    'apikey': 'AZP3JTBNUHAOYIYP'
+}
+
+response = requests.get(url=STOCK_ENDPOINT, params=parameters)
+
+data = response.json()
+
+daily_data = data['Time Series (Daily)']
+
+two_day_data = dict(itertools.islice(daily_data.items(), 2))
+
+two_day_close_list = []
+
+for date in two_day_data:
+    two_day_close_list.append(float(two_day_data[date]['4. close']))
+
+close_diff = abs(two_day_close_list[1] - two_day_close_list[0])
+
+print(close_diff)
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
@@ -11,7 +44,7 @@ COMPANY_NAME = "Tesla Inc"
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
 
 
-#Optional: Format the SMS message like this: 
+# Optional: Format the SMS message like this:
 """
 TSLA: 🔺2%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
@@ -21,4 +54,3 @@ or
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
-
